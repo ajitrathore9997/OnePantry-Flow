7b35b96097407a68ef6440ed8a5ec4f1127b745b
kuldeepClarigogo <kuldeep.clarigoinfotech@gmail.com>
2023-02-21 17:39:14 +0530

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 //Install express server
 const express = require('express');
 const app = express();
-const path = require('path');
+// const path = require('path');
 const port = 3558;
 
 // Serve only the static files form the build directory
-app.use(express.static(__dirname + '/build'));
+// app.use(express.static(__dirname + '/build'));
 
 const http = require('http');
 
-app.get('/*', function (req, res) {
-    return res.sendFile(path.join(__dirname + '/build', 'index.html'));
+// app.get('/*', function (req, res) {
+//     return res.sendFile(path.join(__dirname + '/build', 'index.html'));
+// })
+
+const root = require('path').join(__dirname, 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
 })
 
 const server = http.createServer(app);
@@ -24,3 +30,4 @@ server.listen(port, (err, succ) => {
         console.log('Express server listening on port ' + port);
     }
 });
+
